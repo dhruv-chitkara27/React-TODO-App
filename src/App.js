a7a6507789ca836ad74fbc413254ea630a4b4cfd
Dhruv <dhruvchitkara27@gmail.com>
2019-03-15 23:42:38 +0530

    React Events

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ const TodoItem = props => (
     <Checkbox />
   </Table.Cell>
   <Table.Cell>
-    {props.children}
+    {todo.title}
     <Button
       color="red"
       icon="trash"
       floated="right"
       compact
       size="small"
+      onClick={() => this.handleDelete(todo, i)}
     />
   </Table.Cell>
   </Table.Row>
@@ -29,10 +30,60 @@ class App extends Component {
       {title: 'Learn React Native', completed: false},
       {title: 'Create a brand new web app!', completed: false}
     ],
+    newTodo: '',
+  }
+
+
+  handleTodoClick(todo, index) {
+    const {completed} = todo
+    const [...todos] = this.state.todos
+    console.log({ todos })
+    todos[index] = {
+      ...todo,
+      completed: !completed,
+    }
+    this.setState({ todos })
+  }
+
+  handleInputChange = (event) => {
+    const value = event.target.value
+    this.setState({ newTodo: value})
+  }
+
+  handleNewTodoKeyDown = event => {
+    if (this.state.todos.length >= 10) {
+      //don't allow more than 10 todos
+      return
+    }
+
+    if (event.keyCode !== 13) { // 13 is enter keyCode
+      return
+    }
+    event.preventDefault()
+
+    const {newTodo, todos} = this.state
+    const value = newTodo.trim()
+    if (value) {
+      this.setState ({
+        todos: [
+          ...todos,
+          { title: value, completed: false },
+        ],
+        newTodo: '',
+      })
+    }
+  }
+
+  handleDelete = (todo, i) => {
+    const { todos } = this.state
+    const todosWithoutDeletedTodo = todos.filter(
+      (t, index) => index !== i,
+  )
+  this.setState({ todos: todosWithoutDeletedTodo })
   }
 
   render() {
-
+    const { todos, newTodo } = this.state
     return (
       <div className="app">
       <div className="todo-container">
@@ -41,11 +92,25 @@ class App extends Component {
           className="new-todo"
           placeholder="What needs to be done ?"
           autoFocus
+          value={this.state.newTodo}
+          onChange={this.handleInputChange}
+          onKeyDown={this.handleNewTodoKeyDown}
           />
           <label htmlFor="new-todo" style={{ display: 'none'}}
           >
           New Todo
           </label>
+          {todos.length === 0 ? (
+            <Table>
+            <Table.Header>
+            <Table.Row>
+            <Table.HeaderCell>
+              You have nothing to do!
+            </Table.HeaderCell>
+            </Table.Row>
+            </Table.Header>
+            </Table>
+          ) : (
           <Table>
           <Table.Header>
           <Table.Row>
@@ -63,16 +128,9 @@ class App extends Component {
             <Table.Cell>
               <Checkbox
               checked={todo.completed}
-              onChange={() => {
-                const {completed} = todo
-                const [...todos] = this.state.todos
-                console.log({ todos })
-                todos[i] = {
-                  ...todo,
-                  completed: !completed,
-                }
-                this.setState({ todos })
-              }}
+              onChange={() =>
+                this.handleTodoClick(todo, i)
+              }
               />
             </Table.Cell>
             <Table.Cell>
@@ -88,7 +146,7 @@ class App extends Component {
             </Table.Row>
         ))}
         </Table.Body>
-        </Table>
+        </Table> )}
     </div>
     </div>
 )
